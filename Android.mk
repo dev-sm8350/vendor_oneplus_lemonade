9460179272de634a88d7186dd7763202d8505ee0
TogoFire <togofire@mailfence.com>
2024-11-26 17:51:37 -0300

    lemonade: Drop fw
    It is not cool to include fw in rom. It can cause problems.
    I've seen people losing their device because of FW Prebuilt.
    
    Change-Id: caf3d09f2cf270d3327535c654e2e2ae39e6344e
    Signed-off-by: TogoFire <togofire@mailfence.com>

--- Android.mk
+++ Android.mk
@@ -3,31 +3,3 @@
 #
 
 LOCAL_PATH := $(call my-dir)
-
-ifeq ($(TARGET_DEVICE),lemonade)
-
-$(call add-radio-file-sha1-checked,radio/abl.img,97aa18c4163ef62c7ddcb724b6b8cf9540c3f4f6)
-$(call add-radio-file-sha1-checked,radio/aop.img,13dfe31f3a26c31bdd65f0d6c5a22a9287db0289)
-$(call add-radio-file-sha1-checked,radio/bluetooth.img,4f632ca40f7d8b39586d8fabb277dfd0733f2303)
-$(call add-radio-file-sha1-checked,radio/cpucp.img,64f87c2d20935a98839b83ddb39e5088bee11746)
-$(call add-radio-file-sha1-checked,radio/devcfg.img,7df272368c0b74eb738d07f9f2690e1c213bae37)
-$(call add-radio-file-sha1-checked,radio/dsp.img,634718247c9670c0c2aab842804ea74c3f274396)
-$(call add-radio-file-sha1-checked,radio/engineering_cdt.img,13f5f33df779408b04cdfb3d78dd1ebbd3afb7b4)
-$(call add-radio-file-sha1-checked,radio/featenabler.img,98abcdd3bcfb83fda184d6bb5548585374745151)
-$(call add-radio-file-sha1-checked,radio/hyp.img,8cadeb92f830c81f3f283a4e7be2987d3b4bd16e)
-$(call add-radio-file-sha1-checked,radio/imagefv.img,6d9ff3db65a15f22b24b6cce88c567eaa4e963dd)
-$(call add-radio-file-sha1-checked,radio/keymaster.img,06fa5b1a49a78c094f22772fe5d2b2ea67d1ac1e)
-$(call add-radio-file-sha1-checked,radio/modem.img,2116174f33a6ae91ec092778850b70b2129cfab0)
-$(call add-radio-file-sha1-checked,radio/multiimgoem.img,5f84225b92ad4e4797cf2a213f7cacf66184a9cc)
-$(call add-radio-file-sha1-checked,radio/oplus_sec.img,70c1fb07ea5d63af49361d57b2a5f5d22a7b5612)
-$(call add-radio-file-sha1-checked,radio/oplusstanvbk.img,97c77cf90f3d6c7f821c5765c942aa8302743e0b)
-$(call add-radio-file-sha1-checked,radio/qupfw.img,7835735807c0299922772604bcd7e4b34e33bc7a)
-$(call add-radio-file-sha1-checked,radio/qweslicstore.img,180cf2903b9872499d05eeef08c472575fa3225e)
-$(call add-radio-file-sha1-checked,radio/shrm.img,73fdebc9233548563575c7517b875589243ec974)
-$(call add-radio-file-sha1-checked,radio/splash.img,cef1d4886175660f305e78ab39b7782f8625e431)
-$(call add-radio-file-sha1-checked,radio/tz.img,34a8a6f7a398582641b9cbfeac61f5d88fed9fb6)
-$(call add-radio-file-sha1-checked,radio/uefisecapp.img,079669192c1e389758a858ab72d836702eea067e)
-$(call add-radio-file-sha1-checked,radio/xbl.img,4c3bfd209ac598b86c94647bc544f8fef88fc250)
-$(call add-radio-file-sha1-checked,radio/xbl_config.img,b68b1fbc68a91a6ba86a407b19c3cc284dde8510)
-
-endif
